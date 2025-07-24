82b24d05b60512bae3f521e6a2ce7eb62d8185f2
SanthiyaNehru <sandhiyan345@gmail.com>
2025-07-24 21:03:43 +0530

    Day1 challenges

---
+++ Day1.java
@@ -0,0 +1,29 @@
+package code;
+
+import java.util.Scanner;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		Scanner scan =new Scanner(System.in);
+		System.out.println("Enter n:");
+		int n=scan.nextInt();
+		
+		if(n<0)
+		{
+			System.out.println("Invalid Number!.please enter positive integer.");
+		}
+		else if(n%2==0)
+		{
+			System.out.println(n + " is even number.");
+		}
+		else
+		{
+			System.out.println(n + " is odd number.");
+		}
+		
+		scan.close();
+
+	}
+
+}
